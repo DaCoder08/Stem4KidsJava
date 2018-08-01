45b3a64095e46f3fee297ad0e347b06e3d87a0ab
DaCoder08 <40702668+DaCoder08@users.noreply.github.com>
2018-08-01 14:29:42 -0700

    Main

--- game2048.java
+++ game2048.java
@@ -57,10 +57,6 @@ public class game2048{
                 else if(grid[i][1] == grid[i][2]){
                     grid[i][1] = grid[i][1]*2;
                     grid[i][2] = 0;
-                    if(grid[i][2] == grid[i][3]){
-                        grid[i][2] = grid[i][2]*2;
-                        grid[i][3] = 0;
-                    }
                 }else if(grid[i][2] == grid[i][3]){
                     grid[i][2] = grid[i][2]*2;
                     grid[i][3] = 0;
@@ -68,48 +64,44 @@ public class game2048{
             }
         }
         else if(con.equals("d")){
-            for(int i = 0;i<4;i++){
-                if(grid[i][3] == grid[i][2]){
-                    grid[i][3] = grid[i][3]*2;
-                    grid[i][2] = 0;
-                    if(grid[i][1] == grid[i][0]){
-                        grid[i][1] = grid[i][1]*2;
-                        grid[i][0] = 0;
+            for(int i = 3;i>-1;i--){
+                if(grid[i][0] == grid[i][1]){
+                    grid[i][0] = grid[i][0]*2;
+                    grid[i][1] = 0;
+                    if(grid[i][2] == grid[i][3]){
+                        grid[i][2] = grid[i][2]*2;
+                        grid[i][3] = 0;
                     }
                 }
-                else if(grid[i][2] == grid[i][1]){
-                    grid[i][2] = grid[i][2]*2;
-                    grid[i][1] = 0;
-                    if(grid[i][1] == grid[i][0]){
-                        grid[i][1] = grid[i][1]*2;
-                        grid[i][0] = 0;
-                    }
-                }else if(grid[i][1] == grid[i][0]){
+                else if(grid[i][1] == grid[i][2]){
                     grid[i][1] = grid[i][1]*2;
-                    grid[i][0] = 0;
+                    grid[i][2] = 0;
+                }else if(grid[i][2] == grid[i][3]){
+                    grid[i][2] = grid[i][2]*2;
+                    grid[i][3] = 0;
                 }
             }
         }
         else if(con.equals("w")){
-            for(int i = 0;i<4;i++){
-                if(grid[3][i] == grid[2][i]){
-                    grid[3][i] = grid[3][i]*2;
-                    grid[2][i] = 0;
-                    if(grid[1][i] == grid[0][i]){
-                        grid[1][i] = grid[1][i]*2;
-                        grid[0][i] = 0;
+            for(int i = 3;i>-1;i--){
+                if(grid[0][i] == grid[1][i]){
+                    grid[0][i] = grid[0][i]*2;
+                    grid[1][i] = 0;
+                    if(grid[2][i] == grid[3][i]){
+                        grid[2][i] = grid[2][i]*2;
+                        grid[3][i] = 0;
                     }
                 }
-                else if(grid[2][i] == grid[1][i]){
-                    grid[2][i] = grid[2][i]*2;
-                    grid[1][i] = 0;
-                    if(grid[1][i] == grid[0][i]){
-                        grid[1][i] = grid[1][i]*2;
-                        grid[0][i] = 0;
-                    }
-                }else if(grid[1][i] == grid[0][i]){
+                else if(grid[1][i] == grid[2][i]){
                     grid[1][i] = grid[1][i]*2;
-                    grid[0][i] = 0;
+                    grid[2][i] = 0;
+                    if(grid[2][i] == grid[3][i]){
+                        grid[2][i] = grid[2][i]*2;
+                        grid[3][i] = 0;
+                    }
+                }else if(grid[2][i] == grid[3][i]){
+                    grid[2][i] = grid[2][i]*2;
+                    grid[3][i] = 0;
                 }
             }
         }

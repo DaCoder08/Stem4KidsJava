1d6b5bfab2430f9444c7ac3ea98899cef66cafcc
DaCoder08 <40702668+DaCoder08@users.noreply.github.com>
2018-08-02 11:25:50 -0700

    Main

--- game2048.java
+++ game2048.java
@@ -221,7 +221,7 @@ public class game2048{
         shiftOnce(con);
     }
     public static boolean gameOver(){
-        int[][] temp = grid;
+        int[][] temp = new int[4][4];
         for(int i = 0;i<4;i++){
             for(int j = 0;j<4;j++){
                 temp[i][j] = grid[i][j];
